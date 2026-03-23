0abfc7023c857c0fb3d48c482d6ee81f389eaa47
audit system <other8822personne@gmail.com>
2026-03-23 23:51:13 +0100

    Update db.js

--- db.js
+++ db.js
@@ -2,10 +2,10 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host:            'localhost',
-  database:        'lpa_db',
+  host:            'mysql.railway.internal',
+  database:        'railway',
   user:            'root',
-  password:        '',          // XAMPP/WAMP = vide par défaut
+  password:        'vutyVZqMmAILuQVdtEnmKmorRlhytxkM',          // XAMPP/WAMP = vide par défaut
   waitForConnections: true,
   connectionLimit: 10,
   charset:         'utf8mb4',
